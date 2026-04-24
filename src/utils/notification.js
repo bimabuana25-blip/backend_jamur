6809d868a69d8849a3e9ef3b80c7fe15a0230799
bimabuana <bimabuanao3@gmail.com>
2026-04-24 10:34:03 +0700

    fix: Revert OneSignal authorization scheme back to Basic

--- src/utils/notification.js
+++ src/utils/notification.js
@@ -22,7 +22,7 @@ const sendNotification = async (userId, title, message) => {
       method: "POST",
       headers: {
         "Content-Type": "application/json; charset=utf-8",
-        "Authorization": `Key ${restApiKey}`
+        "Authorization": `Basic ${restApiKey}`
       },
       body: JSON.stringify(payload)
     });
